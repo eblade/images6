b1a7c7116af0ee7538bdf5a44eafd196ea8c03bf
Johan Egneblad <johan@egneblad.se>
2016-05-26 23:41:42 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+name_ = 'images'
+version_ = '6.0.0'
+packages_ = [
+    'images6',
+    'images6.ingest',
+    'exifread',
+    'exifread.tags',
+    'exifread.tags.makernote',
+]
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name=name_,
+    version=version_,
+    author='Johan Egneblad',
+    author_email='johan@DELETEMEegneblad.se',
+    description='Image library and viewer',
+    license="MIT",
+    url='https://github.com/eblade/'+name_,
+    download_url=('https://github.com/eblade/%s/archive/v%s.tar.gz'
+                  % (name_, version_)),
+    packages=packages_,
+    install_requires=[
+        "pillow>=2.5.1",
+        "bottle>0.12.7",
+        "sqlalchemy>=1.0.0",
+    ],
+    classifiers=classifiers,
+)
