bec71502cdcd0cc7a77396a77012a39ff1b9d100
Johan Egneblad <johan@egneblad.se>
2016-12-27 15:30:46 +0100

    Re-add data-id attribute to get bulk tagging to work again

--- web/js/images.viewer.js
+++ web/js/images.viewer.js
@@ -541,6 +541,7 @@ $(function() {
     var add_thumb = function(index, entry) {
         $('#viewer_feed')
             .append('<img data-self-url="' + entry.self_url +
+                    '" data-id="' + entry._id +
                     '" data-state="' + entry.state +
                     '" data-state-url="' + entry.state_url +
                     '" data-check-url="' + entry.check_url +
